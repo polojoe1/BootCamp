309a47895dea0dfe196f0e3cea4e4759071e127b
polojoe1 <98787546+polojoe1@users.noreply.github.com>
2022-06-25 20:19:08 -0700

    Add files via upload

---
+++ loops Challenge/script.js
@@ -0,0 +1,32 @@
+//Print odd 1-20
+/*for(var i=1;i<=20;i++){
+    if(i%2!==0){
+        console.log(i)
+    }
+}*/
+
+//Decreasing Multiples of 3
+/*for(i=100;i>=0;i--){
+    if(i%3===0){
+        console.log(i)
+    }
+}*/
+
+//Print the sequence
+/*for(i=4;i>-4;i-=1.5){
+    console.log(i)
+}*/
+
+//Sigma
+/*var sum = 0;
+for(var i = 1;i<=100;i++){
+    sum = sum + i;
+}
+console.log(sum)*/
+
+//Factorial
+var product = 1;
+for(var i =1;i<=12;i++){
+    product = product * i
+}
+console.log(product)
