90dd9f628bf95e0672a454bd3978b4e1368a32fa
Joseph Coats <coats.joseph30@gmail.com>
2022-06-28 16:29:44 -0700

    coded accept button and edit profile

---
+++ Profile/script.js
@@ -0,0 +1,21 @@
+var h1 = document.querySelector('#Joe');
+function nameGet(){
+    h1.innerText = "The Goat";
+}
+var connects = 2;
+var topNum = document.querySelector("#dos")
+function removeMe(){
+    var raw = document.querySelector('#Top');
+    
+    connects--
+    raw.remove();
+    dos.innerText = connects
+}
+function removeMe2(){
+    var bot = document.querySelector('#Butt');
+    
+    
+    connects--
+    bot.remove();
+    dos.innerText = connects
+}
